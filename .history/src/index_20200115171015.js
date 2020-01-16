e841bc2dead30a61e6b2dee47efa844eb1f46929
Gerardo Cazares <grcazares@gmail.com>
2020-01-15 17:12:38 -0800

    1.2

--- .history/src/index_20200115171015.js
+++ .history/src/index_20200115171015.js
@@ -6,9 +6,4 @@ import 'bootstrap/dist/css/bootstrap.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 
-ReactDOM.render(
-    <Router>
-        <App/>
-    </Router>,
-    document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
